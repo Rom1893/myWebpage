a4cd676abb11c8b5c2bf06ac935823f42d97e6b5
Rodrigo Morán <rodrigobq18@gmail.com>
2022-11-15 22:48:35 -0300

    Added components

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import Header from "./Components/Header/Header"
+import Nav from "./Components/Nav/Nav"
+import About from "./Components/About/About"
+import Experience from "./Components/Experience/Experience"
+import Services from "./Components/Services/Services"
+import Portfolio from "./Components/Portfolio/Portfolio"
+import Testimonials from "./Components/Testimonials/Testimonials"
+import Contact from "./Components/Contact/Contact"
+import Footer from "./Components/Footer/Footer"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
