b7141489bf35aa4a525570da70b36e388cbf1010
Rodrigo Morán <rodrigobq18@gmail.com>
2022-11-17 01:24:44 -0300

    Added Nav Icons

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import {AiOutlineHome, AiOutlineUser} from "react-icons/ai"
+import {BiBook} from "react-icons/bi"
+import {MdOutlineHomeRepairService, MdOutlineContactPhone} from "react-icons/md"
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <nav>
+      <a><AiOutlineHome/></a>
+      <a><AiOutlineUser/></a>
+      <a><BiBook/></a>
+      <a><MdOutlineHomeRepairService/></a>
+      <a><MdOutlineContactPhone/></a>
+    </nav>
   )
 }
