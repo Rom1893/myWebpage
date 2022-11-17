0f1bfcd40c2b1b743906219ef6cab302e1f16a26
Rodrigo Morán <rodrigobq18@gmail.com>
2022-11-17 01:06:23 -0300

    Finishing Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,22 +7,23 @@ import HeaderSocials from './HeaderSocials'
 const Header = () => {
   return (
     <header>
-        <div className="container header__container">
-          <h5>Hello im h5</h5>
-          <h1>Rodrigo Morán</h1>
-          <h5 className='text-light'>FrontEnd Developer</h5>
-          <CTA/>
-          <HeaderSocials/>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Rodrigo Morán</h1>
+        <h5 className='text-light'>Front-end React Developer</h5>
+        <CTA />
+        <HeaderSocials />
 
-          <div className="me">
-            <img src={ME} alt="" />
-          </div>
-
-          <a href="" className='scroll_down'>Scroll Down</a>
+        <div class="box">
+          <span></span>
         </div>
+
+        <a href="" className='scroll__down'>Scroll Down</a>
+      </div>
     </header>
   )
 }
 
 
 export default Header
+
--- src/Components/Header/HeaderSocials.jsx
+++ src/Components/Header/HeaderSocials.jsx
@@ -6,8 +6,8 @@ import {BsGithub} from "react-icons/bs"
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><BsGithub/></a>
+        <a href="https://linkedin.com" target="_blank"><BsLinkedin size={35}/></a>
+        <a href="https://github.com" target="_blank"><BsGithub size={35}/></a>
     </div>
   )
 }
