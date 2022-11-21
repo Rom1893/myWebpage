ec6a256e03cf20ac7e8f80d041ec6c9301ee16d2
Rodrigo Morán <rodrigobq18@gmail.com>
2022-11-21 16:19:58 -0300

    restructured components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Header from "./Components/Header/Header"
 import Nav from "./Components/Nav/Nav"
 import About from "./Components/About/About"
 import Experience from "./Components/Experience/Experience"
-import Services from "./Components/Services/Services"
 import Portfolio from "./Components/Portfolio/Portfolio"
 import Testimonials from "./Components/Testimonials/Testimonials"
 import Contact from "./Components/Contact/Contact"
@@ -17,7 +16,6 @@ function App() {
       <Nav/>
       <About/>
       <Experience/>
-      <Services/>
       <Portfolio/>
       <Testimonials/>
       <Contact/>
--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function Portfolio() {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My recent work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio_item'>
+          <div className="portfolio_item-image"></div>
+          <h3>This is title</h3>
+          <a href="https://github.com" className='btn'>Github</a>
+          <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
