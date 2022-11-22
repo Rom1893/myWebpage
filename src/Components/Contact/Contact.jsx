4e11af97e02288b8d5f3f346716abc461af33bca
Rodrigo Morán <rodrigobq18@gmail.com>
2022-11-22 13:35:51 -0300

    Restyling small media

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import "./Contact.css"
+import { MdOutlineMailOutline } from "react-icons/md"
+import { BsWhatsapp } from "react-icons/bs"
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in touch</h5>
+      <h2>Contact me</h2>
+
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className='contact_option'>
+            <MdOutlineMailOutline className='contact_option-icon' size={40} />
+            <h4>Email</h4>
+            <h5>rodrigobq18@gmail.com</h5>
+            <a href="mailto:rodrigobq18@gmail.com" target="_blank">Send a message</a>
+          </article>
+          <article className='contact_option'>
+            <BsWhatsapp className='contact_option-icon' size={40} />
+            <h4>WhatsApp</h4>
+            <h5>WhatsApp Number</h5>
+            <a href="https://api.whatsapp.com/send?phone=56977422412" target="_blank">Send a message</a>
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Fullname' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="textarea" placeholder='Your message here' cols="30" rows="7" required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
