e4d660dffb8756eef541933c56a6213dd6dc62ea
Rodrigo Morán <rodrigobq18@gmail.com>
2022-11-22 14:00:57 -0300

    Fixed responsive styles

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -10,11 +10,13 @@ export default function Portfolio() {
       <div className="container portfolio__container">
         {PortfolioData.map(m => (
           <article className='portfolio_item' key={m.id}>
-            <div className={`portfolio_image${m.id}`} />
-            <h3>{m.title}</h3>
-            <div className="portoflio-item-cta">
-              <a href="https://github.com" className='btn'>Github</a>
-              <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+            <div className="port">
+              <div className={`portfolio_image${m.id}`} />
+              <h3>{m.title}</h3>
+              <div className="portoflio-item-cta">
+                <a href="https://github.com" className='btn'>Github</a>
+                <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+              </div>
             </div>
           </article>
         ))}
