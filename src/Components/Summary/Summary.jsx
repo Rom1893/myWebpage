0b0e05597c44cb1d47064e3e165c52adf8bb2b51
Rodrigo Morán <rodrigobq18@gmail.com>
2022-11-22 20:19:04 -0300

    Updated summary component

--- src/Components/Summary/Summary.jsx
+++ src/Components/Summary/Summary.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { AiOutlineCheck } from "react-icons/ai"
 
 export default function Summary() {
   return (
-    <section id='Summary'>Summary</section>
+    <section id='Summary'>
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+
+      <div className="container summary_container">
+        <article className='summary'>
+          <div className="summary_head">
+            <h3>UI/UX</h3>
+          </div>
+          <ul className='summary_list'>
+            <li>
+              <AiOutlineCheck />
+              <p>IM GOOD</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
